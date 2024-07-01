5a3194d6effbf47dbb77a1319e15892889a7f345
Yusuf-Babagana <yusufmuhdkk@gmail.com>
2024-07-01 08:39:03 +0000

    :wqRevert "Altered settings.py"
    This reverts commit 7bef3d59251e662661ce366b67a991049a1d9ccc.
    
    :wq#

--- iexam/settings.py
+++ iexam/settings.py
@@ -30,7 +30,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_TIMEZONE = 'UTC'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
